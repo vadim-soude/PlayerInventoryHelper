3d943f9a42f1c2a81494cc19dc3848f198d64ece
Vadim Soudé <vadimsoude@gmail.com>
2026-02-24 15:31:53 +0100

    Initial commit

---
+++ src/main/java/fr/vadimsoude/playerInventoryHelper/PlayerInventoryHelper.java
@@ -0,0 +1,17 @@
+package fr.vadimsoude.playerInventoryHelper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlayerInventoryHelper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
