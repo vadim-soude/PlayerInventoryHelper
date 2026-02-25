6ca7922745ff985f90c1e702432f39f04c2ab9c7
vadim-soude <vadimsoude@gmail.com>
2026-02-25 10:14:44 +0100

    fix : Bukkit event sync state and handlers

--- src/main/java/fr/vadimsoude/playerInventoryHelper/event/PlayerInventoryEvent.java
+++ src/main/java/fr/vadimsoude/playerInventoryHelper/event/PlayerInventoryEvent.java
@@ -1,6 +1,7 @@
 package fr.vadimsoude.playerInventoryHelper.event;
 
 import lombok.Getter;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
@@ -13,11 +14,16 @@ public class PlayerInventoryEvent extends Event {
     private final Player player;
 
     public PlayerInventoryEvent(Player player) {
+        super(!Bukkit.isPrimaryThread());
         this.player = player;
     }
 
     @Override
     public @NotNull HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    public static @NotNull HandlerList getHandlerList(){
         return HANDLERS;
     }
 
